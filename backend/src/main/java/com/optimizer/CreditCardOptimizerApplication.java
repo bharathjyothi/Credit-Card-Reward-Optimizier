6b2eaa01b3987c5787064e954a43d8ea0d860de2
bharathjyothi <bjyothi2002@gmail.com>
2025-06-01 22:47:52 -0400

    springboot setup

---
+++ backend/src/main/java/com/optimizer/CreditCardOptimizerApplication.java
@@ -0,0 +1,11 @@
+package com.optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardOptimizerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCardOptimizerApplication.class, args);
+    }
+}
